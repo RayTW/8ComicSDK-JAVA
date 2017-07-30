93a6e567b639ec9666ff6372f16a4816d7cab115
Ray <ray00000sina@gmail.com>
2017-07-30 15:53:00 +0800

    add : 	定義class R8Comic待實作的method。

--- src/net/xuite/blog/ray00000test/library/comicsdk/R8Comic.java
+++ src/net/xuite/blog/ray00000test/library/comicsdk/R8Comic.java
@@ -1,5 +1,54 @@
 package net.xuite.blog.ray00000test.library.comicsdk;
 
-public class R8Comic {
+import java.util.List;
+import java.util.Map;
 
+public class R8Comic {
+	private static R8Comic sInstance = new R8Comic();
+	private Config mConfig = new Config();
+	private Parser mParser = new Parser();
+	
+	public static R8Comic get(){
+		return sInstance;
+	}
+	
+	/**
+     * 讀取全部漫畫編號、名稱
+     **/
+	public void getAll(OnLoadListener<List<Comic>> listener){
+		//TODO 用Parser解析漫畫名稱後回傳結果
+	}
+	
+	/**
+     * 讀取漫畫簡介、作者、最後更新日期、集數列表
+     **/
+	public void loadComicDetail(Comic comic, OnLoadListener<Comic> listener){
+		//TODO 用Parser解析漫畫資訊後回傳結果
+	}
+	
+	/**
+     * 讀取漫畫簡介、作者、最後更新日期、集數列表
+     **/
+	public void loadEpisodeDetail(Episode episode, OnLoadListener<Episode> listener){
+		//TODO 用Parser解析漫畫資訊後回傳結果
+	}
+	
+	/**
+     * 讀取漫畫圖片實際存放的Server site網址列表
+     **/
+	public void loadSiteUrlList(OnLoadListener<Map<String, String>> listener){
+		
+	}
+	
+	public Parser getParser(){
+		return mParser;
+	}
+	
+	public Parser getmConfig(){
+		return mConfig;
+	}
+	
+	public static interface OnLoadListener<Result>{
+		public abstract void onLoad(Result result);
+	}
 }
