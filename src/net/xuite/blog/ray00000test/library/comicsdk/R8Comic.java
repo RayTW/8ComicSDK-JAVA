1c6be0c2d00bdf2f9e6919a71edae950f97bdcc6
Ray Lee <ray00000sina@gmail.com>
2017-08-11 17:56:21 +0800

    add : 實作讀取漫畫網站html。

--- src/net/xuite/blog/ray00000test/library/comicsdk/R8Comic.java
+++ src/net/xuite/blog/ray00000test/library/comicsdk/R8Comic.java
@@ -1,7 +1,11 @@
 package net.xuite.blog.ray00000test.library.comicsdk;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
+import net.xuite.blog.ray00000test.library.net.EasyHttp;
+import net.xuite.blog.ray00000test.library.net.EasyHttp.Response;
 
 /**
  * 
@@ -28,8 +32,7 @@ public class R8Comic {
      * 讀取全部漫畫編號、名稱
      **/
 	public void getAll(OnLoadListener<List<Comic>> listener){
-		//TODO 用Parser解析漫畫名稱後回傳結果
-		String htmlString = null;
+		String htmlString = requestGetHttp(mConfig.mAllUrl);
 		List<Comic> result = mParser.allComics(htmlString, mConfig);
 		
 		if(listener != null){
@@ -41,8 +44,7 @@ public class R8Comic {
      * 讀取漫畫簡介、作者、最後更新日期、集數列表
      **/
 	public void loadComicDetail(Comic comic, OnLoadListener<Comic> listener){
-		//TODO 用Parser解析漫畫資訊後回傳結果
-		String htmlString = null;
+		String htmlString = requestGetHttp(mConfig.getComicDetailUrl(comic.getId()));
 		Comic result = mParser.comicDetail(htmlString, comic);
 		
 		if(listener != null){
@@ -54,8 +56,7 @@ public class R8Comic {
      * 讀取漫畫簡介、作者、最後更新日期、集數列表
      **/
 	public void loadEpisodeDetail(Episode episode, OnLoadListener<Episode> listener){
-		//TODO 用Parser解析漫畫資訊後回傳結果
-		String htmlString = null;
+		String htmlString = requestGetHttp(episode.getUrl());
 		Episode result = mParser.episodeDetail(htmlString, episode);
 		
 		if(listener != null){
@@ -67,7 +68,7 @@ public class R8Comic {
      * 讀取漫畫圖片實際存放的Server site網址列表
      **/
 	public void loadSiteUrlList(OnLoadListener<Map<String, String>> listener){
-		String htmlString = null;
+		String htmlString = requestGetHttp(mConfig.mCviewJSUrl);
 		Map<String, String> result = mParser.cviewJS(htmlString);
 		
 		if(listener != null){
@@ -85,5 +86,31 @@ public class R8Comic {
 	
 	public static interface OnLoadListener<Result>{
 		public abstract void onLoaded(Result result);
+	}
+	
+
+	private String requestGetHttp(String url){
+		String result = null;
+		EasyHttp request = new EasyHttp.Builder()
+				.setUrl(url)
+				.setMethod("GET").setIsRedirect(true)
+				.setCharset("BIG5")
+				.putHeader("Accept",
+						"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+				.putHeader("Accept-Encoding", "gzip, deflate, br")
+				.setUserAgent(
+						"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36")
+				.build();
+
+		try {
+			Response response = request.connect();
+			result = response.getBody();
+			System.out.println(response.getHeaders());
+			System.out.println("code[" + response.getStatusCode() + "],body==>" + response.getBody());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
