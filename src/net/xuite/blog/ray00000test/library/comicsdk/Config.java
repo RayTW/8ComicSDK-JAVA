ab31ed2340faaad76b5b834fd646cdb5dacc390d
Ray Lee <ray00000sina@gmail.com>
2017-08-11 17:55:30 +0800

    fix : 修改class Config的ststic成員為class成員。

--- src/net/xuite/blog/ray00000test/library/comicsdk/Config.java
+++ src/net/xuite/blog/ray00000test/library/comicsdk/Config.java
@@ -5,22 +5,22 @@ package net.xuite.blog.ray00000test.library.comicsdk;
  * @author Ray Lee Created on 2017/08/11
  */
 public class Config {
-	public static final String sComicHost = "http://www.comicbus.com/";
-	public static final String sAllUrl = sComicHost + "comic/all.html";
-	public static final String sCviewJSUrl = sComicHost + "js/comicview.js";
-	private static final String sSmallIconUrl = sComicHost + "pics/0/%@s.jpg";
-	private static final String sIconUrl = sComicHost + "pics/0/%@.jpg";
-	private static final String sComicDetail = sComicHost + "html/%@.html";
+	public final String mComicHost = "http://www.comicbus.com/";
+	public final String mAllUrl = mComicHost + "comic/all.html";
+	public final String mCviewJSUrl = mComicHost + "js/comicview.js";
+	private final String mSmallIconUrl = mComicHost + "pics/0/%@s.jpg";
+	private final String mIconUrl = mComicHost + "pics/0/%@.jpg";
+	private final String mComicDetail = mComicHost + "html/%@.html";
 
 	public String getComicDetailUrl(String comicId) {
-		return String.format(sComicDetail, comicId);
+		return String.format(mComicDetail, comicId);
 	}
 
 	public String getComicIconUrl(String comicId) {
-		return String.format(sIconUrl, comicId);
+		return String.format(mIconUrl, comicId);
 	}
 
 	public String getComicSmallIconUrl(String comicId) {
-		return String.format(sSmallIconUrl, comicId);
+		return String.format(mSmallIconUrl, comicId);
 	}
 }
